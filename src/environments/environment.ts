692df82d98832d98eaae6f273da4ce700d8e8220
Juan Gomez <gb.jc@outlook.com>
2019-09-12 22:11:51 -0600

    instalación, configuración y primer overview del uso de Firebase Real Time

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCGLLTnLOzP9oOrlleqG2O6LQ4v2v3AMp4",
+    authDomain: "firechat-14a88.firebaseapp.com",
+    databaseURL: "https://firechat-14a88.firebaseio.com",
+    projectId: "firechat-14a88",
+    storageBucket: "",
+    messagingSenderId: "254036652716",
+    appId: "1:254036652716:web:2085f3e1d7c077fb256509"
+
+  }
 };
 
 /*
