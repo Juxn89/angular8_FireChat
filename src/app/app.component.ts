1fc6da25d11ad4481fdc2c7dab25c035f930e4ce
Juan Gomez <gb.jc@outlook.com>
2019-09-19 20:42:15 -0600

    Implementación del logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { ChatService } from './services/chat.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ export class AppComponent {
   title = 'FireChat';
   public chats: Observable<any[]>;
 
-  constructor(db: AngularFirestore) {
+  constructor(db: AngularFirestore, public chatService: ChatService) {
     this.chats = db.collection('chats').valueChanges();
   }
 }
